a3365c670135a48f56e23a83a3d2d971ac2a60b2
Matheus Novo <matheusnovo28@gmail.com>
2023-09-14 17:03:00 -0300

    commit inicial

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production: false
+}
+export const SpotifyConfiguration = {
+    clientId: '9d8c1c53e424485b9e723bf557d5f043',
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+    redirectUrl: 'http://localhost:4200/login/',
+    scopes:[
+        "user-read-currently-playing", // musica tocando agora.
+        "user-read-recently-played", // ler musicas tocadas recentemente
+        "user-read-playback-state", // ler estado do player do usuario
+        "user-top-read", // top artistas e musicas do usuario
+        "user-modify-playback-state", // alterar do player do usuario.
+        "user-library-read", // ler biblioteca dos usuarios
+        "playlist-read-private", // ler playlists privads
+        "playlist-read-collaborative" // ler playlists colaborativas
+    ]
+}
