78b4117fcb8d820754ce2e23fe8ac6c72baf4de2
Matheus Novo <matheusnovo28@gmail.com>
2023-09-16 01:37:01 -0300

    Pegar e utilizar o token gerado pelo Spotify

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor (private spotifyService: SpotifyService){}
-
-  abrirPaginaLogin(){
-    window.location.href = this.spotifyService.obterUrlLogin();
+  ngOnInit(): void {
+    this.verificarTokenUrl();
+    //console.log(this.spotifyService.obterToken());
   }
 
+  verificarTokenUrl(){
+    const token = this.spotifyService.obterToken();
+    if(token != ''){
+      this.spotifyService.definirAccessToken(token);
+    }
+    
+  }
+
+  constructor(private spotifyService: SpotifyService) {}
+
+  abrirPaginaLogin() {
+    window.location.href = this.spotifyService.obterUrlLogin();
+  }
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,20 +1,44 @@
 import { Injectable } from '@angular/core';
 import { SpotifyConfiguration } from 'src/environments/environment.prod';
+import Spotify from 'spotify-web-api-js';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpotifyService {
 
-  constructor() { }
+  spotifyApi: Spotify.SpotifyWebApiJs = null;
 
+  constructor() {
+    this.spotifyApi = new Spotify();
+  }
 
-  obterUrlLogin(){
+  obterUrlLogin() {
     const authEndpoint = `${SpotifyConfiguration.authEndpoint}?`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
-    const scopes = `scopes=${SpotifyConfiguration.scopes.join('%20')}&`;
+    const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}&`;
     const responseType = `response_type=token&show_dialog=true`;
     return authEndpoint + clientId + redirectUrl + scopes + responseType;
+  }
+
+  obterToken() {
+    /* o window.location.hash pega tudo aquilo depois da # na URL
+    e o split('&') divide em um array de strings, separados pelo &
+    Depois eu pego o primeiro item o array e faço um novo split, pra limpar
+    tudo e deixar só o token
+    */
+
+    if(!window.location.hash)
+      return '';
+
+    const paramsUrl = window.location.hash.split('&');
+    const token = paramsUrl[0].split('=')[1];
+    return token;
+  }
+
+  definirAccessToken(token:string){
+    this.spotifyApi.setAccessToken(token);
+    localStorage.setItem("token", token);
   }
 }
